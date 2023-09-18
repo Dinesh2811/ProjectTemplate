240a525d847988b7c23c094522579f3c4dd2555d
Dinesh2811 <dineshk281198@gmail.com>
2023-09-19 03:45:06 +0530

    19-09-23 03:45

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,41 +66,46 @@ android {
 dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.activity:activity-compose:1.7.2")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("com.google.android.material:material:1.11.0-alpha02")
 
     implementation("androidx.compose.material3:material3:1.2.0-alpha07")
     implementation("androidx.compose.material3:material3-android:1.2.0-alpha07")
+    implementation("androidx.compose.material3:material3-window-size-class:1.2.0-alpha07")
 
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 
     implementation(platform("androidx.compose:compose-bom:2023.08.00"))
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
-    testImplementation("junit:junit:4.13.2")
-
     implementation("androidx.compose.ui:ui:1.5.1")
     implementation("androidx.compose.ui:ui-graphics:1.5.1")
     implementation("androidx.compose.ui:ui-tooling-preview:1.5.1")
-    implementation("androidx.compose.material:material:1.5.1")
     implementation("androidx.compose.foundation:foundation:1.5.1")
     implementation("androidx.compose.ui:ui-geometry:1.5.1")
     implementation("androidx.compose.foundation:foundation-layout:1.5.1")
     implementation("androidx.compose.runtime:runtime-livedata:1.5.1")
     implementation("androidx.compose.animation:animation-core:1.5.1")
     implementation("androidx.compose.animation:animation:1.5.1")
-    implementation("androidx.compose.material:material-icons-core:1.5.1")
-    implementation("androidx.compose.material:material-icons-extended:1.5.1")
     implementation("androidx.compose.ui:ui-text:1.5.1")
     implementation("androidx.compose.ui:ui-util:1.5.1")
+    implementation("androidx.compose.ui:ui-viewbinding:1.5.1")
+    implementation("androidx.compose.material:material:1.5.1")
+    implementation("androidx.compose.material:material-icons-core:1.5.1")
+    implementation("androidx.compose.material:material-icons-extended:1.5.1")
 
+    // androidTestImplementation
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.5.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
 
+    // testImplementation
+    testImplementation("junit:junit:4.13.2")
+
+    // debugImplementation
     debugImplementation("androidx.compose.ui:ui-tooling:1.5.1")
     debugImplementation("androidx.compose.ui:ui-test-manifest:1.5.1")
+
 
     //  Room components    	2.5.2   -->  2.6.0-alpha03
     implementation("androidx.room:room-runtime:2.6.0-beta01")
@@ -108,18 +113,12 @@ dependencies {
     implementation("androidx.room:room-ktx:2.6.0-beta01")
 
     //  ViewModel & LiveData
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:2.6.2")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
-
-    // RecyclerView
-    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
-
-    // Navigation Component
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.2")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.2")
 
     // Glide
     implementation("com.github.bumptech.glide:glide:4.16.0")
@@ -137,22 +136,26 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.8")
     implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.8")
 
-    // Lottie
-    implementation("com.airbnb.android:lottie:6.1.0")
-
-    // Volley
-    implementation("com.android.volley:volley:1.2.1")
+    // Navigation Component
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.2")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.2")
+    implementation("androidx.navigation:navigation-compose:2.7.2")  // Navigation Compose
 
     // Paging
     implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
     implementation("androidx.paging:paging-runtime-ktx:3.2.1")
 
+    // Animation
+    implementation("com.airbnb.android:lottie:6.1.0")   // Lottie
+    implementation("com.facebook.shimmer:shimmer:0.5.0")    //  Shimmer
+
     // Location Services
     implementation("com.google.android.gms:play-services-location:21.0.1")
 
+    // RecyclerView
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
 
-    // New
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-    implementation("androidx.compose.material3:material3-window-size-class:1.2.0-alpha07")
-    implementation("androidx.navigation:navigation-compose:2.7.2")  // Navigation Compose
+    // Volley
+    implementation("com.android.volley:volley:1.2.1")
+
 }
