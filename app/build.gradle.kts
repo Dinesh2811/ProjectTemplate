649358662e9e043cf37a3e1645f29f9f606787f3
Dinesh2811 <dineshk281198@gmail.com>
2023-10-29 22:55:24 +0530

    Select ThemePreference

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -83,117 +83,6 @@ android {
 }
 
 dependencies {
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.activity:activity-compose:1.8.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.google.android.material:material:1.11.0-beta01")
-
-    implementation("androidx.compose.material3:material3:1.2.0-alpha10")
-    implementation("androidx.compose.material3:material3-android:1.2.0-alpha10")
-    implementation("androidx.compose.material3:material3-window-size-class:1.2.0-alpha10")
-
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
-
-    implementation(platform("androidx.compose:compose-bom:2023.10.00"))
-    implementation("androidx.compose.ui:ui:1.5.4")
-    implementation("androidx.compose.ui:ui-graphics:1.5.4")
-    implementation("androidx.compose.ui:ui-tooling-preview:1.5.4")
-    implementation("androidx.compose.foundation:foundation:1.5.4")
-    implementation("androidx.compose.ui:ui-geometry:1.5.4")
-    implementation("androidx.compose.foundation:foundation-layout:1.5.4")
-    implementation("androidx.compose.runtime:runtime-livedata:1.5.4")
-    implementation("androidx.compose.animation:animation-core:1.5.4")
-    implementation("androidx.compose.animation:animation:1.5.4")
-    implementation("androidx.compose.ui:ui-text:1.5.4")
-    implementation("androidx.compose.ui:ui-util:1.5.4")
-    implementation("androidx.compose.ui:ui-viewbinding:1.5.4")
-    implementation("androidx.compose.material:material:1.5.4")
-    implementation("androidx.compose.material:material-icons-core:1.5.4")
-    implementation("androidx.compose.material:material-icons-extended:1.5.4")
-
-    // androidTestImplementation
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.10.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.5.4")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-
-    // testImplementation
-    testImplementation("junit:junit:4.13.2")
-
-    // debugImplementation
-    debugImplementation("androidx.compose.ui:ui-tooling:1.5.4")
-    debugImplementation("androidx.compose.ui:ui-test-manifest:1.5.4")
-
-
-    //  Room components    	2.5.2   -->  2.6.0-alpha03
-    implementation("androidx.room:room-runtime:2.6.0")
-    ksp("androidx.room:room-compiler:2.6.0")
-    implementation("androidx.room:room-ktx:2.6.0")
-
-    //  ViewModel & LiveData
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:2.6.2")
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
-
-    // Glide
-    implementation("com.github.bumptech.glide:glide:4.16.0")
-    annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
-
-    // Retrofit
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:retrofit-mock:2.9.0")
-
-    // Gson
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0") // Gson Converter
-    implementation("com.google.code.gson:gson:2.10.1") // Used to convert Java Object into JSON representation
-
-    // HTTP
-    implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.8")
-    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.8")
-
-    // Navigation Component
-    implementation("androidx.navigation:navigation-fragment-ktx:2.7.4")
-    implementation("androidx.navigation:navigation-ui-ktx:2.7.4")
-    implementation("androidx.navigation:navigation-compose:2.7.4")  // Navigation Compose
-
-    // Paging
-    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
-    implementation("androidx.paging:paging-runtime-ktx:3.2.1")
-
-    // Animation
-    implementation("com.airbnb.android:lottie:6.1.0")   // Lottie
-    implementation("com.facebook.shimmer:shimmer:0.5.0")    //  Shimmer
-
-    // Location Services
-    implementation("com.google.android.gms:play-services-location:21.0.1")
-
-    // RecyclerView
-    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
-
-    // Volley
-    implementation("com.android.volley:volley:1.2.1")
-
-    //  Dagger
-    implementation("com.google.dagger:dagger:2.48.1")
-    implementation("com.google.dagger:dagger-android:2.48.1")
-    implementation("com.google.dagger:dagger-android-support:2.48.1")
-    ksp("com.google.dagger:dagger-compiler:2.48.1")
-
-    //  Hilt
-    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
-    implementation("com.google.dagger:hilt-android:2.48.1")
-    ksp("com.google.dagger:hilt-android-compiler:2.48.1")
-    ksp("androidx.hilt:hilt-compiler:1.0.0")
-
-}
-
-/*
-
-dependencies {
     implementation(libs.bundles.android)
     implementation(libs.bundles.compose)
     implementation(platform(libs.androidx.compose.bom))
@@ -240,37 +129,40 @@ dependencies {
     androidTestImplementation(libs.bundles.android.test)
     debugImplementation(libs.bundles.debug)
     testImplementation(libs.bundles.test)
+
+    //  Dagger
+    implementation(libs.bundles.dagger)
+    ksp(libs.dagger.compiler)
+
+    //  Hilt
+    implementation(libs.bundles.hilt)
+    ksp(libs.bundles.hilt.compiler)
 }
 
- */
-
 /*
-libs.versions.toml
-
-
 [versions]
-agp = "8.1.1"
+agp = "8.1.2"
 kotlin = "1.9.10"
 
 core-ktx = "1.12.0"
-activity-compose = "1.7.2"
+activity-compose = "1.8.0"
 appcompat = "1.6.1"
 constraintlayout = "2.1.4"
-material = "1.11.0-alpha03"
+material = "1.11.0-beta01"
 
-material3 = "1.2.0-alpha08"
+material3 = "1.2.0-alpha10"
 kotlinx-coroutines = "1.7.3"
 
-compose-bom = "2023.09.01"
-ui = "1.5.1"
-compose-material = "1.5.1"
+compose-bom = "2023.10.01"
+ui = "1.5.4"
+compose-material = "1.5.4"
 
-ui-test-junit4 = "1.5.1"
+ui-test-junit4 = "1.5.4"
 test-junit = "1.1.5"
 espresso-core = "3.5.1"
 junit = "4.13.2"
 
-room = "2.6.0-rc01"  #   2.5.2   -->   2.6.0-alpha03
+room = "2.6.0"  #   2.5.2   -->   2.6.0-alpha03
 lifecycle = "2.6.2"
 glide = "4.16.0"
 retrofit = "2.9.0"
@@ -279,7 +171,7 @@ okhttp = "5.0.0-alpha.8"
 converter-gson = "2.9.0"
 gson = "2.10.1"
 
-navigation = "2.7.3"
+navigation = "2.7.4"
 lottie = "6.1.0"
 volley = "1.2.1"
 swiperefreshlayout = "1.1.0"
@@ -289,6 +181,11 @@ paging-runtime-ktx = "3.2.1"
 
 play-services-location = "21.0.1"
 shimmer = "0.5.0"
+
+dagger = "2.48.1"
+
+hilt-android = "2.48.1"
+hilt-compiler = "1.0.0"
 
 
 [libraries]
@@ -385,6 +282,18 @@ androidx-swiperefreshlayout = { module = "androidx.swiperefreshlayout:swiperefre
 #  Volley
 volley = { module = "com.android.volley:volley", version.ref = "volley" }
 
+#  Dagger
+dagger = { module = "com.google.dagger:dagger", version.ref = "dagger" }
+dagger-compiler = { module = "com.google.dagger:dagger-compiler", version.ref = "dagger" }
+dagger-android-support = { module = "com.google.dagger:dagger-android-support", version.ref = "dagger" }
+dagger-android = { module = "com.google.dagger:dagger-android", version.ref = "dagger" }
+
+#  Hilt
+hilt-android = { module = "com.google.dagger:hilt-android", version.ref = "hilt-android" }
+androidx-hilt-navigation-compose = { module = "androidx.hilt:hilt-navigation-compose", version.ref = "hilt-compiler" }
+hilt-android-compiler = { module = "com.google.dagger:hilt-android-compiler", version.ref = "hilt-android" }
+hilt-compiler = { module = "androidx.hilt:hilt-compiler", version.ref = "hilt-compiler" }
+
 
 
 [plugins]
@@ -417,5 +326,9 @@ android-test = ["androidx-ui-test-junit4", "androidx-espresso-core", "androidx-j
 debug = ["androidx-ui-tooling", "androidx-ui-test-manifest"]
 test = ["junit"]
 
+dagger = ["dagger", "dagger-android-support", "dagger-android"]
+
+hilt = ["hilt-android", "androidx-hilt-navigation-compose"]
+hilt-compiler = ["hilt-android-compiler", "hilt-compiler"]
 
  */
