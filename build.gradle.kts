6fe06069e503d83b7101f5e6393a22598510638b
Dinesh2811 <dineshk281198@gmail.com>
2024-02-12 00:15:56 +0530

    1.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,14 +5,23 @@ plugins {
 //    alias(libs.plugins.ksp) apply false
 //    alias(libs.plugins.hiltAndroid) apply false
 
-    id("com.android.application") version "8.2.1" apply false
-    id("com.android.library") version "8.2.1" apply false
+    id("com.android.application") version "8.2.2" apply false
+    id("com.android.library") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 
     id("com.google.devtools.ksp") version "1.9.22-1.0.17" apply false
     id("com.google.dagger.hilt.android") version "2.50" apply false
 }
 
-//val compileSdk by extra(34)     //  rootProject.extra["compileSdk"] as Int
-//val kotlinCompilerExtensionVersion by extra("1.5.8")    //  rootProject.extra["kotlinCompilerExtensionVersion"] as String
+val compileSdk by extra(34)     //  rootProject.extra["compileSdk"] as Int
+val targetSdk by extra(34)     //  rootProject.extra["targetSdk"] as Int
+val jvmToolchain by extra(17)     //  rootProject.extra["jvmToolchain"] as Int
+val kotlinCompilerExtensionVersion by extra("1.5.9")    //  rootProject.extra["kotlinCompilerExtensionVersion"] as String
 
+val isReleaseMinifyEnabled by extra(true)     //  rootProject.extra["isReleaseMinifyEnabled"] as Boolean
+val isReleaseShrinkResources by extra(true)     //  rootProject.extra["isReleaseShrinkResources"] as Boolean
+val isReleaseTestCoverage by extra(false)     //  rootProject.extra["isReleaseTestCoverage"] as Boolean
+
+val isDebugMinifyEnabled by extra(false)     //  rootProject.extra["isDebugMinifyEnabled"] as Boolean
+val isDebugShrinkResources by extra(false)     //  rootProject.extra["isDebugShrinkResources"] as Boolean
+val isDebugTestCoverage by extra(true)     //  rootProject.extra["isDebugTestCoverage"] as Boolean
